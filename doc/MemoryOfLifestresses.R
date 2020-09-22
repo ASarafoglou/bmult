97e4596f9935e5e5189d21b115f3daa22a57d773
Alexandra Sarafoglou <alexandra.sarafoglou@gmail.com>
2020-09-22 13:07:11 +0200

    changed folder structure

---
+++ doc/MemoryOfLifestresses.R
@@ -0,0 +1,45 @@
+## -----------------------------------------------------------------------------
+library(bridgeineq)
+data(lifestresses)
+lifestresses
+
+# visualize the data
+op <- par(cex.main = 1.5, mar = c(5, 6, 4, 5) + 0.1, mgp = c(3.5, 1, 0), cex.lab = 1.5 , font.lab = 2, cex.axis = 1.3, bty = "n", las = 1)
+  plot(lifestresses$month, lifestresses$stress.freq, col = "black", 
+       pch = 1, cex = 2, type="b",
+       xlim = c(0, 18), ylim = c(0, 20), 
+       ylab = "", xlab = "", axes = FALSE)
+  axis(1, at = seq(0, 18, length.out = 10), label = seq(0, 18, length.out = 10))
+  axis(2, at = seq(0, 20, length.out = 5), label = seq(0, 20, length.out = 5), las = 1) 
+  par(las = 0)
+  mtext("Months Before Interview", side = 1, line = 2.5, cex = 1.5)
+  mtext("Participants Reporting a\nNegative Life Event", side = 2, line = 3.0, cex = 1.5)
+
+## -----------------------------------------------------------------------------
+categories <- lifestresses$month
+x          <- lifestresses$stress.freq
+
+# Prior specification 
+# We assign a uniform Dirichlet distribution, that is, we set all concentration parameters to 1
+alpha <- rep(1, 18)
+
+# Test the following restricted Hypothesis:
+# Hr: month1 > month2 > ... > month18 
+Hr   <- paste0(1:18, collapse=">"); Hr
+
+## ---- cache = TRUE------------------------------------------------------------
+ineq_results <- multBayesInformed(categories, Hr=Hr, a=alpha, counts=x, bf_type = 'BFre', seed = 2020)
+
+## -----------------------------------------------------------------------------
+summary(ineq_results)
+
+## -----------------------------------------------------------------------------
+ineq_results$bf_list
+ineq_bayesfactors <- ineq_results$bf_list$bf
+
+## ---- cache = TRUE------------------------------------------------------------
+eq_results      <- bridgeineq::multBfEquality(a=alpha, counts=x)
+eq_bayesfactors <- eq_results$bf
+
+BFr0 <- ineq_bayesfactors[['BFre']] * eq_bayesfactors[['BFe0']]; BFr0
+
---
+++ doc/MendelianLawsOfInheritance.R
@@ -0,0 +1,24 @@
+## -----------------------------------------------------------------------------
+library(bridgeineq)
+data(peas)
+peas
+
+## -----------------------------------------------------------------------------
+categories <- peas$peas
+x          <- peas$counts
+
+# Prior specification 
+# We assign a uniform Dirichlet distribution, that is, we set all concentration parameters to 1
+alpha <- c(1, 1, 1, 1)
+
+# Test the following restricted Hypothesis:
+# Hr: month1 > month2 > ... > month18 
+Hr   <- c('roundYellow > wrinkledYellow = roundGreen > wrinkledGreen')
+
+## ---- cache = TRUE------------------------------------------------------------
+results <- bridgeineq::multBayesInformed(categories, Hr=Hr, a=alpha, counts=x, bf_type = 'BFre', seed = 2020)
+
+## -----------------------------------------------------------------------------
+summary(results)
+# summary(ineq_results)$equalities$bf
+
